e1cdcfd2cb2f8fd2aad9f4f4a60cf9e0a9acf97b
StandicP <standic@seznam.cz>
2023-12-20 17:44:25 +0100

    Přidání řádku print na konec - pokus

---
+++ Prepocet_jednotek.py
@@ -0,0 +1,18 @@
+print(20*"=")
+
+kg_pocet=int(input("Zadej počet kg: "))
+km_pocet=int(input("Zadej počet km: "))
+l_pocet=int(input("Zadej počet l: "))
+
+print(20*"=")
+
+kg_vysledek=(kg_pocet*2.2)
+print(kg_pocet, "kg je po přepočtu:", kg_vysledek, "lb")
+km_vysledek=(km_pocet*0.62)
+print(km_pocet, "km je po přepočtu:", km_vysledek, "mil")
+l_vysledek=(l_pocet*0.26)
+print(l_pocet, "l je po přepočtu:", l_vysledek, "gal")
+
+print(20*"=")
+
+print("Přidání textu - pokus pro Git")
