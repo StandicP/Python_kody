e3711cf6f5d5edf264575c35fbbbcb9aee098529
StandicP <standic@seznam.cz>
2023-12-26 12:26:53 +0100

    Doplnění

--- Prepocet_jednotek.py
+++ Prepocet_jednotek.py
@@ -1,4 +1,6 @@
 print(20*"=")
+print("PŘEPOČET JEDNOTEK")
+print(20*"=")
 
 kg_pocet=int(input("Zadej počet kg: "))
 km_pocet=int(input("Zadej počet km: "))
@@ -6,15 +8,11 @@ l_pocet=int(input("Zadej počet l: "))
 
 print(20*"=")
 
-kg_vysledek=(kg_pocet*2.2)
+kg_vysledek=round((kg_pocet*2.2), 1)
 print(kg_pocet, "kg je po přepočtu:", kg_vysledek, "lb")
-km_vysledek=(km_pocet*0.62)
+km_vysledek=round((km_pocet*0.62), 1)
 print(km_pocet, "km je po přepočtu:", km_vysledek, "mil")
-l_vysledek=(l_pocet*0.26)
+l_vysledek=round((l_pocet*0.26), 1)
 print(l_pocet, "l je po přepočtu:", l_vysledek, "gal")
 
 print(20*"=")
-
-print("Tom")
-
-print("Přidání textu 2 - pokus pro Git 2")
